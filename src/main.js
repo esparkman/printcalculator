58d941b8c27f7d0479e92b222ce7fb1f5e11fcda
Evan Sparkman <me@evansparkman.com>
2020-05-11 00:03:31 -0400

    Add vue-gtag fot analytics

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueGtag from 'vue-gtag'
 import '@/assets/stylesheets/tailwind.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtag, {
+  config: {
+    id: 'G-QTEGC6WNB7'
+  }
+})
 
 new Vue({
   router,
